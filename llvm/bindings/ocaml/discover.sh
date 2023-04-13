4475d90eb345f62bc0b8ba9dc3ec97aff54e3e92
Alan Hu <alanh@ccs.neu.edu>
2023-04-13 12:56:23 -0400

    Stash WIP code

---
+++ llvm/bindings/ocaml/discover.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+set -e
+set -x
+
+if test "$(dirname $0)" != '.'; then
+    echo "The script must be executed from its current directory."
+    exit 1
+fi
+
+if test "$#" -ne 1; then
+    echo "Usage: $0 <llvm-config>"
+    exit 1
+fi
+
+llvm_config=$1
+default_mode=
+support_static_mode=false
+support_shared_mode=false
+
+llvm_config() {
+    "$llvm_config" $@
+}
+
+llvm_version=$(llvm_config --version | cut -d. -f1)
+expected_version=$(cat VERSION | cut -d. -f1)
+if test "$llvm_version" != "$expected_version"; then
+  echo "LLVM version does not match. Expected '$expected_version' but got '$llvm_version'"
+  exit 1
+fi
+
+if llvm_config --link-static --libs; then
+    default_mode=static
+    support_static_mode=true
+fi
+
+if llvm_config --link-shared --libs; then
+    default_mode=shared
+    support_shared_mode=true
+fi
+
+if test -z "$default_mode"; then
+    echo "Something is wrong with the llvm-config command provided."
+    exit 1
+fi
+
+base_cflags=$(llvm_config --cflags)
+ldflags="$(llvm_config --ldflags) -lstdc++"
+llvm_targets=$(llvm_config --targets-built)
+
+echo "($base_cflags)" > "c_flags.sexp"
+echo "($ldflags)" > "c_library_flags.sexp"
